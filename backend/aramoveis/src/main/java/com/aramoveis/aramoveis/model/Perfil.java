40806fc429a751d4f043b83e6991e847e6210a16
Matheus <matheus.de.souza.code@gmail.com>
2025-07-09 19:16:14 -0300

    auth-service add

--- backend/aramoveis/src/main/java/com/aramoveis/aramoveis/model/Perfil.java
+++
@@ -1,7 +0,0 @@
-package com.aramoveis.aramoveis.model;
-
-public enum Perfil {
-    ENCARREGADO,
-    CONFERENTE,
-    AUXILIAR
-}
--- backend/aramoveis/src/main/java/com/aramoveis/aramoveis/repository/ItemAdministrativoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aramoveis.aramoveis.repository;
-
-import com.aramoveis.aramoveis.model.ItemAdministrativo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemAdministrativoRepository extends JpaRepository<ItemAdministrativo, String> {
-}
